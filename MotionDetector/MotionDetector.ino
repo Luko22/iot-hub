10e5dfd9328fb53372f744a214c16905e0c1205b
Luko <89535176+Luko22@users.noreply.github.com>
2026-01-15 12:02:21 +0100

    Added a MotionDetector Circuit
    its a mini survaillance project with an esp32Cam + HC-SR501 PIR module maybe with future api capabilities

---
+++ MotionDetector/MotionDetector.ino
@@ -0,0 +1,151 @@
+#include <credentials.h> //to hide sensitive credentials
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+#include "esp_camera.h"
+#include "esp_http_server.h"
+#include <ESP32Servo.h>
+
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+
+#include "camera_pins.h"
+
+const char *ssid = mySSID; // values sotred in credentials.h
+const char *password = myPASSWORD ;
+
+void startCameraServer();
+void setupLedFlash(int pin);
+
+const int pirPin = 12;
+const int ledPin =4;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  ///wifi setup
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.setSleep(false);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+
+  initCamera();
+  startCameraServer();
+
+  Serial.print("Camera Ready! Use 'http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("' to connect");
+
+
+
+   
+  pinMode(pirPin,INPUT);
+  pinMode(ledPin,OUTPUT);
+
+  signalBlink(ledPin);
+
+
+  Serial.println("Code is ready. The video stream is located in the URL: ");
+  String u = "http://"+WiFi.localIP().toString()+":81/stream";//goes directly into video feed, not the camsera server UI
+  Serial.print(u);
+ 
+  delay(1000);
+}//end of setup
+
+void loop() {
+
+  int motion = digitalRead(pirPin);
+  digitalWrite(ledPin, motion);
+  delay(100);
+
+
+}//end of loop/////////////////////////////////
+
+
+
+void initCamera() {
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+
+  if (psramFound()) {
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+    config.grab_mode = CAMERA_GRAB_LATEST;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.fb_location = CAMERA_FB_IN_DRAM;
+  }
+
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed: 0x%x\n", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_hmirror(s, 1);
+  s->set_vflip(s, 1);
+
+  if (s->id.PID == OV3660_PID) {
+    s->set_brightness(s, 1);
+    s->set_saturation(s, -2);
+  }
+
+  s->set_framesize(s, FRAMESIZE_SVGA);
+
+  #if defined(LED_GPIO_NUM)
+    setupLedFlash(LED_GPIO_NUM);
+  #endif
+}
+
+void signalBlink(int pin) {
+  digitalWrite(pin, HIGH);
+  delay(50);
+  digitalWrite(pin, LOW);
+  delay(50);
+  digitalWrite(pin, HIGH);
+  delay(50);
+  digitalWrite(pin, LOW);
+  delay(50);
+  digitalWrite(pin, HIGH);
+  delay(50);
+  digitalWrite(pin, LOW);
+  delay(50);
+  digitalWrite(pin, HIGH);
+  delay(50);
+  digitalWrite(pin, LOW);
+  delay(50);
+}
